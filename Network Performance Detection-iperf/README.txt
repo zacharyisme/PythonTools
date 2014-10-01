8519b830b64efcf36cb7c787c776c11b0c03bdde
Zachary Huang <zacharyisme@gmail.com>
2014-10-01 16:38:24 +0800

    Python_iperf
    python script to run iperf on each client and generate html formate
    email to send to admin.

---
+++ Network Performance Detection-iperf/README.txt
@@ -0,0 +1,14 @@
+python-iperf-source: 原始檔
+python-iperf-Final:經修改符合unalis之用檔
+
+
+此Script用於產生iperf 網路工具產生之結果報告之用。
+使用步驟：
+1. 安裝及設定iperf伺服器端
+2. 安裝iperf用戶端
+3. 將用戶端執行程式所產生的結果導入文件檔TXT
+4. 透過此script抓取文件檔資料並透過email寄發報告
+
+參考網站：
+http://www.haogongju.net/art/1898619
+http://www.cnblogs.com/aguncn/archive/2013/03/26/2983284.html
---
+++ Network Performance Detection-iperf/python-iperf-Final.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+import sys
+import os.path
+import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.utils import formatdate
+
+
+folder = 'D:\iperf\log\\'
+msg_low_sh = ''
+msg_normal = ''
+msg_low_us = ''
+msg_nodata = ''
+
+for filename in os.listdir (folder):
+    if (filename.split('.')[1] != "txt") :
+        continue
+    statinfo = os.stat(folder + filename)
+    if (statinfo.st_size == 0):
+        msg_nodata += "<tr color='#4c4c4c'  bgcolor='#4c4c4c' height = '20'><td>" + filename.split('.')[0] + "</td><td>.have now data.</td></tr>\n"
+        continue
+    fp = open(folder + filename,'r')
+    for i, line in enumerate(fp):
+        if (i == 6):
+            speednumber = line.split()
+            if (float(speednumber[6])) < 500 and (float(speednumber[6])) > 400:
+               msg_low_sh += "<tr color=\"#ff6600\" bgcolor='ff6600' height = '20'><td>" + filename.split('.')[0] + "</td><td> Busy Speed: " + speednumber[6] + "Mbits. We should monitor this machine.</td></tr>\n"
+               continue
+            if (float(speednumber[6])) <400:
+               msg_low_us +=  "<tr color=\"#ff0000\" bgcolor='ff0000'  height = '20'><td>" + filename.split('.')[0] + "</td><td>Low Speed: " + speednumber[6] + "Mbits. We should notice this machine.</td></tr>\n"
+               continue
+            else:
+              msg_normal +=  "<tr color=\"#036803\" bgcolor='036803' height = '20'><td>" + filename.split('.')[0] + "</td><td> Normal Speed: " + speednumber[6] + "Mbits.</td></tr>\n"
+    fp.close()
+#print (msg_low_sh)
+#print (msg_low_us)
+#print (msg_normal)
+#print (msg_nodata)
+
+text_file = open("LANSpeedOutput.txt", "w")
+text_file.write('<table width="800" border="1"  bordercolor="#000000">')
+text_file.write(msg_low_sh)
+text_file.write(msg_low_us)
+text_file.write(msg_normal)
+text_file.write(msg_nodata)
+text_file.write('</table>')
+
+text_file.close()
+
+# Define these once; use them twice!
+strFrom = "iperf@company.com.tw"
+#strTo = ["a","b","c"]
+strTo = ["abc@company.com.tw"]
+date = datetime.datetime.now().strftime( "%d/%m/%Y " )
+
+# Create the root message and fill in the from, to, and subject headers
+msgRoot = MIMEMultipart('related')
+msgRoot['Subject'] = "LAN Speed log at "+date
+msgRoot['From'] = strFrom
+msgRoot['To'] = ",".join(strTo)
+msgRoot.preamble = 'This is a multi-part message in MIME format.'
+
+# Encapsulate the plain and HTML versions of the message body in an
+# 'alternative' part, so message agents can decide which they want to display.
+msgAlternative = MIMEMultipart('alternative')
+msgRoot.attach(msgAlternative)
+
+msgText = MIMEText('This is the alternative plain text message.')
+msgAlternative.attach(msgText)
+
+# We reference the image in the IMG SRC attribute by the ID we give it below
+fptxt = open(r'LANSpeedOutput.txt', 'r')
+msgText = MIMEText('<img src="cid:image1"><font size = 4 > Iperf Intranet Speed Report  '+ date +'</font><br>'+fptxt.read(), 'html')
+msgAlternative.attach(msgText)
+fptxt.close()
+
+# This example assumes the image is in the current directory
+fp = open('logo.jpg', 'rb')
+msgImage = MIMEImage(fp.read())
+fp.close()
+
+# Define the image's ID as referenced above
+msgImage.add_header('Content-ID', '<image1>')
+msgRoot.attach(msgImage)
+
+# Send the email (this example assumes SMTP authentication is required)
+import smtplib
+smtp = smtplib.SMTP()
+smtp.connect('192.168.0.1')
+#smtp.login('user', 'password') 
+smtp.sendmail(strFrom, strTo, msgRoot.as_string())
+smtp.quit()
